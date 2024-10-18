2e864b9f6e372a703e3b09e55cb1ba5257cd3398
minaiba <aibarchynkadyrbaeva@gmail.com>
2024-10-18 22:19:47 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+
+for (let i = 0; i <= 100; i++) {
+    if(i % 3 === 0) {
+        console.log("coca", i );
+  }  else if(i % 5 === 0) {
+  console.log('cola', i);
+  }  else if(i % 1 === 0) {
+    console.log(i);
+  }       
+}
